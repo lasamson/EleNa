9202f81e0c549523edac4fa66622efdfb4821ada
Aarsh <aarsh@1x-nat-vl930-172-30-93-41.wireless.umass.edu>
2018-12-05 17:56:58 -0500

    Moved files into their respective folders

--- src/MainInterface.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainInterface from './MainInterface';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MainInterface />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
