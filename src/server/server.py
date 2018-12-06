20666746b7e4a2f3521919a470f8a2924603ef80
Aarsh <aarsh@1x-nat-vl930-172-30-93-41.wireless.umass.edu>
2018-12-05 19:35:27 -0500

    Started writing the flask server code

---
+++ src/server/server.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/test', methods=["GET"])
+def test():
+    print("testing")
+    return "hello"
+
+@app.route("/get_route", methods=["POST"])
+def get_route():
+    content = request.get_json()
+    source = content["Source"]
+    destination = content["Destination"]
+    max_min = content["Max_min"]
+    percentage = content["Percentage"]
+
+    print("Sending Data to Algorithm...")
+    print("Source: {}".format(source))
+    print("Destination: {}".format(destination))
+    print("Max_min: {}".format(max_min))
+    print("Percentage: {}".format(percentage))
+
+    print("Getting optimal route...")
+    print("Sending route to the MapView...")
+    return "Optimal Route"
+
+if __name__ == "__main__":
+    app.run(port=8080)
