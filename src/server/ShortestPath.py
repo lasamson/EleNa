d78f0adbf14e850db61f55ec986f45542b51e08c
Varun Sharma <varunsharmadps@gmail.com>
2018-12-07 18:08:04 -0500

    Minor changes

--- src/server/ShortestPath.py
+++ src/server/ShortestPath.py
@@ -23,28 +23,3 @@ from itertools import count
 
 
 def get_shortest_path(graph, source, target, weight='length'):
-    frontier = []
-    heappush(frontier, (0, source))
-    came_from = {}
-    cost_so_far = {}
-    came_from[source] = None
-    cost_so_far[source] = 0
-
-    while len(frontier) != 0:
-        (val, current) = heappop(frontier)
-        if current == target:
-            break
-        for u, nxt, data in graph.edges(current, data=True):
-            new_cost = cost_so_far[current]
-            if weight == 'length':
-                incCost = self.getcost(graph, u, nxt)
-            elif weight == 'elevation':
-                incCost = self.getelevationcost(graph, u, nxt)
-            if incCost > 0:
-                new_cost += incCost
-            if nxt not in cost_so_far or new_cost < cost_so_far[nxt]:
-                cost_so_far[nxt] = new_cost
-                priority = new_cost
-                heappush(frontier, (priority, nxt))
-                came_from[nxt] = current
-    return self.getpath(came_from, source, target)
