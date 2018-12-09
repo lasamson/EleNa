b76a5e1cd64be89d3803285e3fbc891f794bb3ac
Aarsh <aarsh@Aarshs-MacBook-Pro.local>
2018-12-09 01:25:36 -0500

    You can view the route based on the source and destination added

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,19 +1,35 @@
 // @flow
 
 import React, { Component } from 'react'
-import { Map, TileLayer, Marker, Popup } from '../map_components'
+import { Map, TileLayer, Marker, Popup, Polyline, } from '../map_components'
 
-type State = {
-  lat: number,
-  lng: number,
-  zoom: number,
-}
 
-export default class MapView extends Component<{}, State> {
-  state = {
-    lat: 10.389910,
-    lng: -72.527820,
-    zoom: 13,
+export default class MapView extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      initial_lat: 42.373222,
+      initial_lng: -72.519852,
+      renderRoute: false,
+      mapCenter: [42.36, -71.36],
+      zoom: 14
+    };
+  }
+
+  componentWillReceiveProps(props) {
+    let route = props["route"];
+    console.log(route);
+
+    this.setState({
+      renderRoute: true,
+      mapCenter: route[0],
+      route: route,
+      initial_lat: route[0][0],
+      initial_lng: route[0][1],
+      end_lat: route[route.length-1][0],
+      end_lng: route[route.length-1][1]
+    });
   }
 
   componentWillMount() {
@@ -24,9 +40,29 @@ export default class MapView extends Component<{}, State> {
   }
 
   render() {
-    const position = [this.state.lat, this.state.lng]
+    let initial_position = [this.state.initial_lat, this.state.initial_lng]
+    let route;
+    let initial_marker;
+    let end_marker;
+
+    if(this.state.renderRoute) {
+      route = <Polyline positions={this.state.route} weight={9} opacity={.5} />
+      initial_marker = <Marker key={`marker-1`} position={[this.state.initial_lat, this.state.initial_lng]}>
+        <Popup>
+          <span>Starting Point</span>
+        </Popup>
+     </Marker>;
+      end_marker = <Marker key={`marker-2`} position={[this.state.end_lat, this.state.end_lng]}>
+          <Popup>
+            <span>Destination Point</span>
+          </Popup>
+      </Marker>
+    } else {
+      route = <Marker position={initial_position}></Marker>;
+    }
+
     return (
-      <Map center={position} zoom={this.state.zoom} style={{
+      <Map center={initial_position} zoom={this.state.zoom} style={{
         weight: 2,
         opacity: 1,
         color: 'white',
@@ -37,21 +73,19 @@ export default class MapView extends Component<{}, State> {
         <TileLayer
           url="https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {route}
+        {initial_marker}
+        {end_marker}
       </Map>
     )
   }
 
   setPosition(position) {
     this.setState({
-      lat: position.coords.latitude,
-      lng: position.coords.longitude,
+      initial_lat: position.coords.latitude,
+      initial_lng: position.coords.longitude,
       zoom: 13
-    })
+    });
   }
 
   // style() {
