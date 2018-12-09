79dc5dbe0aa45a93faa5df36c3e8a51eea10fbe4
Chenhao Huang <chenhaohuang96@gmail.com>
2018-12-08 23:34:39 -0500

    added teardown for GetMaptest

--- test/MakeMapsTest.py
+++ test/MakeMapsTest.py
@@ -10,10 +10,16 @@ class MakeMapsTest(unittest.TestCase):
         self.assertEqual(self.map.nodes[5637885552]['elevation']
                          , 99,
                          'incorrect elevation')
+
     def test_output_value(self):
         self.assertEqual(self.map.nodes[5637885552]
                          , {'y': 42.3850694, 'x': -72.5143306, 'elevation': 99, 'osmid': 5637885552},
                          'incorrect value')
+    def tearDown(self):
+        os.remove('AmherstMA.pkl')
+        os.remove('AmherstMA_projected.pkl')
+
 
 if __name__ == '__main__':
     unittest.main()
+
