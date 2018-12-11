7859f427cc0597d2a649af8f61f425bd484dfe71
Chenhao Huang <chenhaohuang96@gmail.com>
2018-12-11 00:03:15 -0500

    change function testin

--- src/server/Controllor.py
+++ src/server/Controllor.py
@@ -10,7 +10,7 @@ def find_route(city, state, start, end, percentage):
         startN = Algorithm.get_closest_node(G, start)
         endN = Algorithm.get_closest_node(G, end)
 
-        p = Algorithm.get_from_all_paths(G, startN, endN, percentage, max_ele=False)
+        p = Algorithm.get_from_djikstra(G, startN, endN, percentage, max_ele=False)
 
         return Algorithm.get_lat_long(G, p)
     else:
