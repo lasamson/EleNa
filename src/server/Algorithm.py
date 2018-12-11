c1716a429232a71b7e94bfdaa375df0daa770949
Chenhao Huang <chenhaohuang96@gmail.com>
2018-12-11 04:30:19 -0500

    fixed get_from_djikstra

--- src/server/Algorithm.py
+++ src/server/Algorithm.py
@@ -1,4 +1,3 @@
-
 import osmnx as ox
 import pickle as pkl
 from pathlib import Path
@@ -44,6 +43,17 @@ def is_gps_in_map(G, (lat, lng)):
 
 def get_closest_node(G, (lat, lng)):
     return ox.get_nearest_node(G, (lat, lng))
+
+
+def getpath(revPath, origin, destination):
+    route_by_length_minele = []
+    p = destination
+    route_by_length_minele.append(p)
+    while p != origin:
+        p = revPath[p]
+        route_by_length_minele.append(p)
+    route_by_length_minele = route_by_length_minele[::-1]
+    return route_by_length_minele
 
 
 def generate_path(revPath, start, end):
@@ -96,16 +106,16 @@ def get_shortest_path(G, start, end, option='length'):
         if current == end:
             break
         for cur, nxt, data in G.edges(current, data=True):
-            new_cost = cost[current]
+            cur_cost = cost[current]
             if option == 'length':
                 curCost = get_length(G, cur, nxt)
             elif option == 'elevation':
                 curCost = get_path_elevation(G, cur, nxt)
             if curCost > 0:
-                new_cost += curCost
-            if nxt not in cost or new_cost < cost[nxt]:
-                cost[nxt] = new_cost
-                heappush(queue, (new_cost, nxt))
+                cur_cost += curCost
+            if nxt not in cost or cur_cost < cost[nxt]:
+                cost[nxt] = cur_cost
+                heappush(queue, (cur_cost, nxt))
                 revPath[nxt] = current
 
     return generate_path(revPath, start, end)
@@ -115,7 +125,7 @@ def get_euclidean_distance(G, start, end):
     x1, y1 = G.nodes()[start]['x'], G.nodes()[start]['y']
     x2, y2 = G.nodes()[end]['x'], G.nodes()[end]['y']
 
-    dist = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+    dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
     return dist
 
@@ -152,95 +162,40 @@ def get_dis_from_percentage(min_distance, percent):
         return (percent) / 100.0 * min_distance
     return (percent) * min_distance
 
+
+
 def get_from_djikstra(G, start, end, percent, max_ele=True):
     min_distance = get_path_length(G, get_shortest_path(G, start, end))
-    shortest_paths = list(nx.all_shortest_paths(G, start, end))
     max_path_length = get_dis_from_percentage(min_distance, percent)
-
-    elevation_gain = {}
-
+    #
     queue = []
     heappush(queue, (0, start))
     revPath = {}
-    visited_cost = {}
-    ele = {}
+    cost = {}
+    cost_ele = {}
     revPath[start] = None
-    visited_cost[start] = 0
-    ele[start] = 0
-
-    while len(queue) > 0:
-        (val, current) = heappop(queue)
-        if current == end:
-            if visited_cost < max_path_length:
+    cost[start] = 0
+    cost_ele[start] = 0
+    while len(queue) != 0:
+        (val, cur) = heappop(queue)
+        if cur == end:
+            if cost[cur] <= max_path_length:
                 break
-        for cur, nextnode, data in G.edges(current, data=True):
-            cost = visited_cost[cur] + get_length(G, cur, nextnode)
-            cur_elev = ele[cur]
-            ele_cost = get_elevation_gain(G, cur, nextnode)
-            if ele_cost > 0:
-                cur_elev += ele_cost
-            if nextnode not in visited_cost or cost < visited_cost[nextnode]:
-                visited_cost[nextnode] = cur_elev
-                ele[nextnode] = cost
-                priority = cur_elev
-                if max_ele == False:
-                    priority = priority
-                heappush(queue, (priority, nextnode))
-                revPath[nextnode] = current
-
+        for cur, next, data in G.edges(cur, data=True):
+            cur_cost = cost[cur] + get_length(G, cur, next)
+            cur_ecost = cost_ele[cur]
+            ecost = get_elevation_gain(G, cur, next)
+            if ecost > 0:
+                cur_ecost = cur_ecost + ecost
+            if next not in cost or cur_cost < cost[next]:
+                cost_ele[next] = cur_ecost
+                cost[next] = cur_cost
+                if max_ele:
+                    priority = -cur_ecost
+                else:
+                    priority = cur_ecost
+                heappush(queue, (priority, next))
+                revPath[next] = cur
     return generate_path(revPath, start, end)
 
-def astar_path(G, source, target, percentage, max_ele=False):
-    heuristic = None
-    if source not in G or target not in G:
-        msg = 'Either source {} or target {} is not in G'
-        raise nx.NodeNotFound(msg.format(source, target))
 
-    if heuristic is None:
-        # The default heuristic is h=0 - same as Dijkstra's algorithm
-        def heuristic(u, v):
-            return 0
-
-    push = heappush
-    pop = heappop
-
-    c = 1
-    queue = [(0, c, source, 0, None)]
-
-    enqueued = {}
-    explored = {}
-
-    while queue:
-        # Pop the smallest item from queue.
-        _, _, curnode, dist, parent = pop(queue)
-
-        if curnode == target:
-            path = [curnode]
-            node = parent
-            while node is not None:
-                path.append(node)
-                node = explored[node]
-            path.reverse()
-            return path
-
-        if curnode in explored:
-            continue
-
-        explored[curnode] = parent
-
-        for neighbor, w in G[curnode].items():
-            if neighbor in explored:
-                continue
-            # print(neighbor, w[0])
-            ncost = dist + w[0]['length']
-            if neighbor in enqueued:
-                qcost, h = enqueued[neighbor]
-                if qcost <= ncost:
-                    continue
-            else:
-                h = heuristic(neighbor, target)
-            enqueued[neighbor] = ncost, h
-            c += 1
-            push(queue, (ncost + h, c, neighbor, ncost, curnode))
-
-    raise nx.NetworkXNoPath("Node %s not reachable from %s" % (source, target))
--- src/server/Controllor.py
+++ src/server/Controllor.py
@@ -10,18 +10,12 @@ def find_route(city, state, start, end, percentage):
         startN = Algorithm.get_closest_node(G, start)
         endN = Algorithm.get_closest_node(G, end)
 
-        p = Algorithm.dfs_get_all_paths(G, startN, endN, 3000)
+        p = Algorithm.get_from_djikstra(G, startN, endN, percentage,True)
         print Algorithm.get_path_length(G,p)
         print Algorithm.get_path_elevation(G,p)
 
-        p = Algorithm.get_all_paths_search(G, startN, endN, percentage, max_ele=True)
-        print Algorithm.get_path_length(G,p)
-        print Algorithm.get_path_elevation(G,p)
-
-        p = Algorithm.astar_path(G, startN, endN, percentage, max_ele=False)
-        return Algorithm.get_lat_long(G, p)
     else:
         print "not in map"
 
-print (find_route("Amherst","MA", (42.374794, -72.518929),
- (42.398794, -72.52805) ,130))
+print (find_route("Amherst","MA", (42.383851, -72.528598),
+ (42.399529, -72.528260) ,200))
